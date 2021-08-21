8fc62b3ac7f03294d81a3c145415428a99911bcf
adesh3963 <67481704+adesh3963@users.noreply.github.com>
2021-08-21 21:29:42 +0530

    Update settings.py

--- spamclassifier/settings.py
+++ spamclassifier/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '4xv7hx#8yi_lt6sf&uc!tbw3=mxch(4(katya@@)i9j@6e&*qg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['spam-test-app.herokuapp.com']
 
 
 # Application definition
